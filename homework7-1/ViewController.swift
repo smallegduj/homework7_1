0bcbcd8d33cab00601387db119ddb75022920d27
MikeWu <MikeWu@mikewude-macbook-pro.local>
2016-05-05 03:52:53 +0800

    畫面上加入text view & label。限制text view只能輸入10個字，無法輸入第11個字。輸入的同時，label同步顯示目前字數

--- homework7-1/ViewController.swift
+++ homework7-1/ViewController.swift
@@ -1,15 +1,28 @@
-//
-//  ViewController.swift
-//  homework7-1
-//
-//  Created by MikeWu on 2016/5/2.
-//  Copyright © 2016年 Mike. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet var countLabel: UILabel!
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        print("old text:\(textView.text)")
+        print("range location:\(range.location)")
+        print("range length:\(range.length)")
+        print("new text:\(text)")
+        
+        let finalString = (textView.text as NSString).stringByReplacingCharactersInRange(range, withString: text)
+        let count = finalString.characters.count
+        if count > 10 {
+            return false
+        }
+        else {
+            self.countLabel.text = "\(count)"
+            return true
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
